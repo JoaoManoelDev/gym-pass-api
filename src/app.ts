c661346bbd270089f261b86db6c3b339f1655cdd
João Manoel <joaomanoeldev@gmail.com>
2024-04-20 20:07:49 -0300

    style: add zod to top

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import fastify from "fastify"
+import { ZodError } from "zod"
 
 import { appRoutes } from "@/http/routes"
-import { ZodError } from "zod"
 import { env } from "@/env"
 
 export const app = fastify()
