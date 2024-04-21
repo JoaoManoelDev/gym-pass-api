eac142e113d9a768757d0df2fa8ccd5d58b662eb
João Manoel <joaomanoeldev@gmail.com>
2024-04-21 00:06:50 -0300

    test: create test to register use case

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,60 @@
-import { expect, test } from "vitest"
+import { describe, expect, it } from "vitest"
 
-test("Check if it works", () => {
-  expect(1 + 1).toBe(2)
+import { RegisterUseCase } from "./register"
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
+import { comparePassword } from "@/utils/hash"
+import {
+  InMemoryUsersRepository
+} from "@/repositories/in-memory/in-memory-users-repository"
+
+describe("Register Use Case", () => {
+  it("should be able to register", async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      password: "123456"
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it("should hash user password upon registration", async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      password: "123456"
+    })
+
+    const isPasswordCorrectlyHashed = await comparePassword({
+      password: "123456",
+      hash: user.password_hash
+    })
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it("should not be able to register with same email twice", async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = "johndoe@email.com"
+
+    await registerUseCase.execute({
+      name: "John Doe",
+      email,
+      password: "123456"
+    })
+
+    expect(() => registerUseCase.execute({
+      name: "John Doe",
+      email,
+      password: "123456"
+    })).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
 })
