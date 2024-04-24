2e19a26e542727af5c12c89523a126d1b0dbe366
João Manoel <joaomanoeldev@gmail.com>
2024-04-24 14:13:30 -0300

    feat: create CheckInUseCase

---
+++ src/use-cases/check-in.ts
@@ -0,0 +1,28 @@
+import { CheckIn } from "@prisma/client"
+
+import { CheckInsRepository } from "@/repositories/check-ins-repository"
+
+interface CheckInRequest {
+  userId: string
+  gymId: string
+}
+
+interface CheckInResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId
+  }: CheckInRequest): Promise<CheckInResponse> {
+    const checkIn = await this.checkInsRepository.create({
+      user_id: userId,
+      gym_id: gymId,
+    })
+
+    return { checkIn }
+  }
+}
