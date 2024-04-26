91679ed8df111cc1114b853cc9e7506fd8cf4d8f
João Manoel <joaomanoeldev@gmail.com>
2024-04-26 17:45:39 -0300

    feat: implement create method to InMemoryGymsRepository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,8 +1,26 @@
-import { Gym } from "@prisma/client"
+import { randomUUID } from "node:crypto"
+import { Gym, Prisma } from "@prisma/client"
+
 import { GymsRepository } from "../gym-repository"
 
 export class InMemoryGymsRepository implements GymsRepository {
   public gyms: Gym[] = []
+
+  async create(gym: Prisma.GymCreateInput) {
+    const newGym = {
+      id: gym.id ?? randomUUID(),
+      title: gym.title,
+      description: gym.description ?? null,
+      phone: gym.phone ?? null,
+      latitude: new Prisma.Decimal(gym.latitude.toString()),
+      longitude: new Prisma.Decimal(gym.longitude.toString()),
+      created_at: new Date()
+    }
+
+    this.gyms.push(newGym)
+
+    return newGym
+  }
 
   async findById(id: string) {
     const gym = this.gyms.find(gym => gym.id === id)
