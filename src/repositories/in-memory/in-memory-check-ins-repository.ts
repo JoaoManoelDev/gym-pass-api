2512b7c67b4a93a23d437317816298f3967b3d00
João Manoel <joaomanoeldev@gmail.com>
2024-04-26 18:54:44 -0300

    feat: implement findManyByUserId to InMemoryCheckInsRepository

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -37,4 +37,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkOnSameDate
   }
+
+  async findManyByUserId(userId: string, page: number) {
+    return this.checkIns
+      .filter(checkIn => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
 }
