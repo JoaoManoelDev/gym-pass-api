308f02462dc1c4e2b9f7a80fa34fcc80c4500621
João Manoel <joaomanoeldev@gmail.com>
2024-04-26 17:45:07 -0300

    feat: add create method to GymsRepository

--- src/repositories/gym-repository.ts
+++ src/repositories/gym-repository.ts
@@ -1,5 +1,6 @@
-import { Gym } from "@prisma/client"
+import { Gym, Prisma } from "@prisma/client"
 
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
+  create(gym: Prisma.GymCreateInput): Promise<Gym>
 }
