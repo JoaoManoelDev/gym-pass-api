b1eec6ab8539b6f0d15a36dba200b293643bb5b1
João Manoel <joaomanoeldev@gmail.com>
2024-04-26 19:17:51 -0300

    feat: implement countByUserId to InMemoryCheckInsRepository

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -43,4 +43,8 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       .filter(checkIn => checkIn.user_id === userId)
       .slice((page - 1) * 20, page * 20)
   }
+
+  async countByUserId(userId: string) {
+    return this.checkIns.filter(checkIn => checkIn.user_id === userId).length
+  }
 }
