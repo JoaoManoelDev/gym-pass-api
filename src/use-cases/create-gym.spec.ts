f216fd15ab30e2591b9eb1c76549e8f92bcb46a9
João Manoel <joaomanoeldev@gmail.com>
2024-04-26 17:46:32 -0300

    test: create test to should be able to create gym

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,28 @@
+import { beforeEach, describe, expect, it } from "vitest"
+
+import {
+  InMemoryGymsRepository
+} from "@/repositories/in-memory/in-memory-gyms-repository"
+import { CreateGymUseCase } from "./create-gym"
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe("Create Gym Use Case", () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it("should be able to create gym", async () => {
+    const { gym } = await sut.execute({
+      title: "NodeJs Gym",
+      description: null,
+      phone: null,
+      latitude: 22.860852,
+      longitude: -43.2406528,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
