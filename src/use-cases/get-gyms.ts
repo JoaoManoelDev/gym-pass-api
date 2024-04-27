ec4774d9ca4ab6c6fba5c871608abefaa686e2c0
João Manoel <joaomanoeldev@gmail.com>
2024-04-27 01:00:36 -0300

    feat: create GetGymsUseCase

---
+++ src/use-cases/get-gyms.ts
@@ -0,0 +1,25 @@
+import { Gym } from "@prisma/client"
+
+import { GymsRepository } from "@/repositories/gym-repository"
+
+interface GetGymsUseCaseRequest {
+  query: string
+  page: number
+}
+
+interface GetGymsUseCaseResponse {
+  gyms: Gym[]
+}
+
+export class GetGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page
+  }: GetGymsUseCaseRequest): Promise<GetGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.getMany(query, page)
+
+    return { gyms }
+  }
+}
