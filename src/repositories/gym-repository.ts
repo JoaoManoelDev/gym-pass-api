cf9e04050d62ba5f35eecfba2fc9c3b002a4be67
João Manoel <joaomanoeldev@gmail.com>
2024-04-27 00:59:58 -0300

    feat: create getMany method

--- src/repositories/gym-repository.ts
+++ src/repositories/gym-repository.ts
@@ -2,5 +2,6 @@ import { Gym, Prisma } from "@prisma/client"
 
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
+  getMany(query: string, page: number): Promise<Gym[]>
   create(gym: Prisma.GymCreateInput): Promise<Gym>
 }
