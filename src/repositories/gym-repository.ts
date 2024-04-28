51838a75240e57db6dce151c2e69db3f1f34e8a2
João Manoel <joaomanoeldev@gmail.com>
2024-04-28 16:05:08 -0300

    feat: create findManyNearby method

--- src/repositories/gym-repository.ts
+++ src/repositories/gym-repository.ts
@@ -1,7 +1,13 @@
 import { Gym, Prisma } from "@prisma/client"
 
+export interface FindManyNearbyParams {
+  latitude: number
+  longitude: number
+}
+
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
+  findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>
   getMany(query: string, page: number): Promise<Gym[]>
   create(gym: Prisma.GymCreateInput): Promise<Gym>
 }
