a4d97a1f02ba46a5510efa1f5f5989a1fb43eff9
João Manoel <joaomanoeldev@gmail.com>
2024-05-09 16:47:42 -0300

    refactor: modify id params to userId

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,7 +3,7 @@ import { CheckIn, Prisma } from "@prisma/client"
 export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   save(checkIn: CheckIn): Promise<CheckIn>
-  findById(id: string): Promise<CheckIn | null>
+  findById(userId: string): Promise<CheckIn | null>
   findByUserIdOnDate(userId: string, data: Date): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   countByUserId(userId: string): Promise<number>
