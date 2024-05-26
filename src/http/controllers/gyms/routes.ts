b189a682f925393dc4638f7845caf386a491ec61
João Manoel <joaomanoeldev@gmail.com>
2024-05-25 21:16:37 -0300

    feat: create routes to create gym and get nearby gyms

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from "fastify"
 
 import { verifyJWT } from "@/http/middlewares/verify-jwt"
+import { search } from "./search"
+import { nearby } from "./nearby"
+import { create } from "./create"
 
 export const gymsRoutes = async (app: FastifyInstance) => {
   app.addHook("onRequest", verifyJWT)
+
+  app.get("/gyms/search", search)
+  app.get("/gyms/nearby", nearby)
+  
+  app.post("/gyms", create)
 }
