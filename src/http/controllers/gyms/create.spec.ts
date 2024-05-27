a5015798ecd383099f778c11a6eca3f93eccea45
João Manoel <joaomanoeldev@gmail.com>
2024-05-27 01:04:31 -0300

    test: create test to should be able to create a gym

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,33 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest"
+import request from "supertest"
+
+import { app } from "@/app"
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user"
+
+describe("Create Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async() => {
+    await app.close()
+  })
+ 
+  it("should be able to create a gym", async () => {
+
+    const { token } = await createAndAuthenticateUser(app)
+    
+    const response = await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "NodoJS Gym",
+        description: "Some Description",
+        phone: "912345678",
+        latitude: 22.860852,
+        longitude: -43.2406528,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
