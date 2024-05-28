d7c1b98003a00d4c0db1ae59bbae4a92a2ec73bc
João Manoel <joaomanoeldev@gmail.com>
2024-05-28 14:33:37 -0300

    feat: add RBAC to routes

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify"
 
 import { verifyJWT } from "@/http/middlewares/verify-jwt"
+import { verifyUserRole } from "@/http/middlewares/verify-user-role"
 import { create } from "./create"
 import { validate } from "./validate"
 import { history } from "./history"
@@ -13,5 +14,10 @@ export const checkInsRoutes = async (app: FastifyInstance) => {
   app.get("/check-ins/metrics", metrics)
 
   app.post("/gyms/:gymId/check-ins", create)
-  app.patch("/check-ins/:checkInId/validate", validate)
+
+  app.patch(
+    "/check-ins/:checkInId/validate",
+    {onRequest: [verifyUserRole("ADMIN")]},
+    validate
+  )
 }
